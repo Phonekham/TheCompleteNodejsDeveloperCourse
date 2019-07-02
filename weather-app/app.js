2b42d3c4b8b44e2b8fb9f4a9566cf11ab63a8cb5
Phonekham <phonekham.dev@gmail.com>
2019-07-02 22:32:22 +0700

    11. Callback Chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,16 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-// geocode("new york ", (error, data) => {
-//   console.log("Error", error);
-//   console.log("Data", data);
-// });
-
-forecast(-75.7088, 44.1545999999999, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
+geocode("vientiane", (error, data) => {
+  if (error) {
+    return console.log(error);
+  } else {
+    forecast(data.latitude, data.longtitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  }
 });
