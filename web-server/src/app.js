0546be02edcf4dde9a943cfbd2f04115014ec98e
Phonekham <phonekham.dev@gmail.com>
2019-07-04 15:54:45 +0700

    5. Serving up CSS, JS, Images, and More

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,5 @@
 const path = require("path");
 const express = require("express");
-console.log(__dirname);
-console.log(path.join(__dirname, "../public"));
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
