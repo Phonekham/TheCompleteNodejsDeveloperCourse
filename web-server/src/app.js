3e567be9be7b8823adb8e1b6411570c227d6ccbe
Phonekham <phonekham.dev@gmail.com>
2019-07-04 15:43:57 +0700

    4. Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,11 @@
+const path = require("path");
 const express = require("express");
+console.log(__dirname);
+console.log(path.join(__dirname, "../public"));
 const app = express();
-app.get("", (req, res) => {
-  res.send("hello express");
-});
 
-app.get("/help", (req, res) => {
-  res.send("Help page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("about page");
-});
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
   res.send("weather page");
