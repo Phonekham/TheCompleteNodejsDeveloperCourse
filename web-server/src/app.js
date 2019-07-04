65d400b559a9ec70259a45dfcc4bfc660d2e1423
Phonekham <phonekham.dev@gmail.com>
2019-07-04 16:27:04 +0700

    6. Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,23 @@ const express = require("express");
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "test",
+    name: "Phonekham"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/help", (req, res) => {
+  res.render("help");
+});
 
 app.get("/weather", (req, res) => {
   res.send("weather page");
